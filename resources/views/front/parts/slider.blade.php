c321a02aa9602889a58fecb50f3b61b3688ac616
AdelAbdelaziz-Ad <a.ghareb@ad.net.sa>
2020-08-20 12:09:01 +0300

    test commit

--- resources/views/front/parts/slider.blade.php
+++ resources/views/front/parts/slider.blade.php
@@ -11,21 +11,21 @@
     </div>
 </div>
 <!-- Hero Section End -->
-  
+
 <!-- Start Search Section -->
 <div id="search-bar" class="col-12 mt-n5">
     <div class="container p-1 pr-0 pl-0">
         <form method="GET" action="https://inaday.cloud/search" accept-charset="UTF-8" class="formsearch">
             <div class="row m-1 ">
                 <div class="col-12 col-md-4 pt-1 pb-1">
-                    {{ Form::open(['action' => 'FrontController@SearchIndex','method' => 'get','class'=>'formsearch']) }}     
+                    {{ Form::open(['action' => 'FrontController@SearchIndex','method' => 'get','class'=>'formsearch']) }}
                     {!! Form::text('query', null, ['required','class' => 'form-control search','id'=>'search','placeholder'=>trans('file.search_for')]) !!}
                     {{ Form::close() }}
                 </div>
                 <div class="col-12 col-md-3  pt-1 pb-1">
                     <select name="section_id" class="form-control required" id="service" required="required" aria-required="true">
-                        <option value="">اختر التصنيف</option>
-                        <option value="1"> خدمات متقدمة</option>
+                        <option value="">اختر المدينة</option>
+                        <option value="1">الرياض</option>
                   </select>
                 </div>
                 <div class="col-12 col-md-3  pt-1 pb-1">
