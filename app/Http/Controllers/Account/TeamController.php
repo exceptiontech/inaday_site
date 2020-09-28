ced2054da8de287229a349123ff28982210ea8ec
Ahmed Elhadi <a.alhadi@ad.net.sa>
2020-09-28 11:21:37 +0300

    fixed issue

--- app/Http/Controllers/Account/TeamController.php
+++ app/Http/Controllers/Account/TeamController.php
@@ -54,7 +54,7 @@ class TeamController extends Controller
             return view('front.errors.denied');
         }
 
-        if (!Auth::user()->userdetailComplete) {
+        if (Auth::user()->userdetailComplete) {
             Session::flash('status', __('admin.info'));
             Session::flash('message', 'لا بد من تحديث الملف الشخصى لتتمكن من اضافة فريق');
             return redirect::to('/account/profile/edit');
