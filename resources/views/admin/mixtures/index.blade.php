6665229ba15d0aefed9c6bf75b3aa338f341b0ae
Ahmed Elhadi <a.alhadi@ad.net.sa>
2020-10-20 15:03:26 +0300

    edit logs

--- resources/views/admin/mixtures/index.blade.php
+++ resources/views/admin/mixtures/index.blade.php
@@ -60,14 +60,52 @@
                                 </th>
 
                                 <td class="actions" width="200">
-                                  <a class="btn btn-info" href="{{ action('Admin\MixtureController@edit',$mixture->id) }}" data-toggle="tooltip" title="{{trans('admin.edit')}}">
+                                <a class="btn btn-secondary" href="{{ action('MixtureController@show',$mixture->id) }}" data-toggle="tooltip" title="{{trans('admin.edit')}}">
+                                    {{trans('admin.show')}}
+                                  </a>
+
+                                  <a class="btn btn-primary" href="{{ action('Admin\MixtureController@approve',$mixture->id) }}" data-toggle="tooltip" title="{{trans('admin.approve')}}">
+                                    {{trans('admin.approve')}}
+                                  </a>
+
+                                  <a class="btn btn-danger" data-toggle="modal" data-target="#item{{$mixture->id}}">
+                                    {{trans('admin.refuse')}}
+                                  </a>
+
+                                    <div class="modal fade" id="item{{$mixture->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog" role="document">
+                                        {{ Form::open(['action' => 'Admin\MixtureController@refuse']) }}
+                                        <div class="modal-content">
+
+                                          <div class="modal-body">
+
+                                            {!! Form::hidden('model_id', $mixture->id, ['required', 'class' => 'form-control', 'readonly' => 'readonly']) !!}
+
+                                            <label>{{trans('admin.refuse_reason')}}</label>
+                                            {!! Form::textarea('desc', null,  array('required', 'class'=>'textarea form-control', 'rows'=>'4')) !!}
+
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">{{trans('admin.close')}}</button>
+
+                                            {!! Form::submit(trans('admin.add'), array('class'=>'btn btn-primary ml-2')) !!}
+                                          </div>
+                                        </div>
+
+                                        {{ Form::close() }}
+                                      </div>
+                                    </div>   
+
+
+
+<!--                                   <a class="btn btn-info" href="{{ action('Admin\MixtureController@edit',$mixture->id) }}" data-toggle="tooltip" title="{{trans('admin.edit')}}">
                                     {{trans('admin.edit')}}
                                   </a>
                                   {{ Form::open(array('url' => 'admin/mixtures/' . $mixture->id,'style'=>'display:inline')) }}
                                       {{ Form::hidden('_method', 'DELETE') }}
                                       {!! Form::button(trans('admin.delete'), array('class' => 'btn btn-danger','data-toggle'=>'tooltip','type'=>'submit', 'title'=>trans('admin.delete'))) !!}
                                   {{ Form::close() }}
-
+ -->
 
                                 </td>
                             </tr>
