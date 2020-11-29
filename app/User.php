98b06f81b96e40bb234ee973f85a1566267d052a
Ahmed Elhadi <a.alhadi@ad.net.sa>
2020-11-29 11:54:26 +0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function isAdmin()
     {
-        if (!$this->roles) {
+        if (is_null($this->roles) {
             return false;
         }
 
@@ -76,7 +76,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function isEntrepreneur()
     {
-        if (!$this->roles) {
+        if (is_null($this->roles) {
             return false;
         }
 
@@ -88,7 +88,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function isServicesProvider()
     {
-        if (!$this->roles) {
+        if (is_null($this->roles) {
             return false;
         }
         if ($this->roles->first()->name == "services_provider" ) {
