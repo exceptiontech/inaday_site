a5348c15d7510cf210e00250966af0195ede52b5
Ahmed Elhadi <a.alhadi@ad.net.sa>
2020-12-13 15:40:23 +0300

    fix review

--- app/Review.php
+++ app/Review.php
@@ -8,6 +8,6 @@ class Review extends Model
 {
     public function user()
     {
-        return $this->belongsTo('App\User', 'user_id');
+        return $this->belongsTo('App\User', 'author_id');
     }
 }
--- resources/views/front/user/reviews.blade.php
+++ resources/views/front/user/reviews.blade.php
@@ -64,8 +64,8 @@
                                                     <div class="col-12 project pb-3 pt-2">
                                                         <h2>{{$review->title ?? 'تقييم'}}</h2>
                                                         <p>{{$review->review}}</p>
+                                                        <p>{{$review->user->first_name . ' ' .A$review->user->last_name}}</p>
 
-                                                        </div>
                                                     </div>
                                                 @endforeach
                                             @else
