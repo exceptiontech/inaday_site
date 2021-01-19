657c79a63e2b1ae89417b6de3ae47adcdf32594c
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-01-19 15:21:57 +0300

    Update create.blade.php

--- resources/views/front/profile/mixtures/create.blade.php
+++ resources/views/front/profile/mixtures/create.blade.php
@@ -110,7 +110,7 @@
 
                                         @if(count($team->users) > 0 )
                                             @foreach($team->users as $user)
-                                                {{$user->confirmServices()}}
+                                                
                                                 @foreach($user->confirmServices() as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3">
                                                     <div class="col-2 align-middle">
@@ -137,7 +137,6 @@
                                             @endforeach
 
                                             @if(count($team->user->confirmServices()) > 0)
-                                                {{$team->user->confirmServices()}}
                                                 @foreach($team->user->confirmServices() as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3">
                                                     <div class="col-2 align-middle">
