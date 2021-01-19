544291393236c97bb85ccf68316b78fae2bf3c2e
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-01-19 15:24:28 +0300

    fix

--- resources/views/front/profile/mixtures/create.blade.php
+++ resources/views/front/profile/mixtures/create.blade.php
@@ -110,8 +110,8 @@
 
                                         @if(count($team->users) > 0 )
                                             @foreach($team->users as $user)
-                                                
-                                                @foreach($user->confirmServices() as $service)
+
+                                                @foreach($user->confirmServices as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3">
                                                     <div class="col-2 align-middle">
                                                         {!! Form::label('service_name', trans('forms.service_name'))!!}
@@ -136,8 +136,8 @@
                                                 @endforeach
                                             @endforeach
 
-                                            @if(count($team->user->confirmServices()) > 0)
-                                                @foreach($team->user->confirmServices() as $service)
+                                            @if(count($team->user->confirmServices > 0)
+                                                @foreach($team->user->confirmServices as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3">
                                                     <div class="col-2 align-middle">
                                                         {!! Form::label('service_name', trans('forms.service_name'))!!}
