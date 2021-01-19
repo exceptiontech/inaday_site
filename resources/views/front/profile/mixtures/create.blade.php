0bf83a67414ca12135d400086f96cdf067d92a87
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-01-19 15:26:13 +0300

    fix

--- resources/views/front/profile/mixtures/create.blade.php
+++ resources/views/front/profile/mixtures/create.blade.php
@@ -110,8 +110,9 @@
 
                                         @if(count($team->users) > 0 )
                                             @foreach($team->users as $user)
+                                                {{$user->confirmServices()}}
 
-                                                @foreach($user->confirmServices as $service)
+                                                @foreach($user->services as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3">
                                                     <div class="col-2 align-middle">
                                                         {!! Form::label('service_name', trans('forms.service_name'))!!}
@@ -136,8 +137,9 @@
                                                 @endforeach
                                             @endforeach
 
-                                            @if(count($team->user->confirmServices > 0)
-                                                @foreach($team->user->confirmServices as $service)
+                                            @if(count($team->user->services) > 0)
+                                                {{$team->user->confirmServices()}}
+                                                @foreach($team->user->services as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3">
                                                     <div class="col-2 align-middle">
                                                         {!! Form::label('service_name', trans('forms.service_name'))!!}
--- resources/views/front/profile/mixtures/edit.blade.php
+++ resources/views/front/profile/mixtures/edit.blade.php
@@ -107,7 +107,7 @@
 
                                         @if(count($mixture->team->users) > 0 )
                                             @foreach($mixture->team->users as $user)
-                                                @foreach($user->confirmServices as $service)
+                                                @foreach($user->services as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3 @if(in_array($service->id, $mixture->services->pluck('id')->toArray() )) bg-primary border-0 @endif">
                                                     <div class="col-2 align-middle">
                                                         {!! Form::label('service_name', trans('forms.service_name'))!!}
@@ -132,8 +132,8 @@
                                                 @endforeach
                                             @endforeach
 
-                                            @if(count($mixture->team->user->confirmServices > 0)
-                                                @foreach($mixture->team->user->confirmServices as $service)
+                                            @if(count($mixture->team->user->services) > 0)
+                                                @foreach($mixture->team->user->services as $service)
                                                 <div class="d-flex border-bottom pb-3  mb-3 @if(in_array($service->id, $mixture->services->pluck('id')->toArray() )) bg-primary border-0 @endif">
                                                     <div class="col-2 align-middle">
                                                         {!! Form::label('service_name', trans('forms.service_name'))!!}
