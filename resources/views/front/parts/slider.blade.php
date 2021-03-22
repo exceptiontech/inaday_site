31978029c3fb23271e4bede4217a8a964ad9b03f
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-03-22 14:04:46 +0300

    Update slider.blade.php

--- resources/views/front/parts/slider.blade.php
+++ resources/views/front/parts/slider.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
         <div class="row">
             <div class="hero-content text-white col-12 col-md-6">
-                <h2 class="pb-5 mb-5">{!!trans('file.slider_title')!!}</h2>
-                <p  class="mb-5">{!!trans('file.slider_desc')!!}</p>
+                <h2 class="pb-4 mb-2">{!!trans('file.slider_title')!!}</h2>
+                <p  class="mb-2">{!!trans('file.slider_desc')!!}</p>
                 @guest
                     <a class="btn btn-primary" href="{{ url('register') }}">{{trans('file.free_start')}}</a>
                 @endguest
