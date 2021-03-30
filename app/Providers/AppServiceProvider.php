164ec538f5fede0d545e03f26b5725eb11a90441
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-03-30 16:58:06 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if(env('APP_ENV') == 'production') {
+            resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        }
+
         Schema::defaultStringLength(191);
     }
 }
