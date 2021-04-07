123c34136e364817d65a74e93fff5c1cfc016a1b
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-04-07 17:03:01 +0300

    Update PassportController.php

--- app/Http/Controllers/API/PassportController.php
+++ app/Http/Controllers/API/PassportController.php
@@ -62,24 +62,24 @@ class PassportController extends Controller
 
         }
 
-        // $requests = $request->all();
-        // $requests['password'] = Hash::make($requests['password']);
-        // $requests['is_active'] = 1;
+        $requests = $request->all();
+        $requests['password'] = Hash::make($requests['password']);
+        $requests['is_active'] = 1;
 
 
-        $user = New User;
-        $user->name = $request->first_name.' '.$request->last_name;
-        $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->email = $request->email;
-        $user->mobile = $request->mobile;
-        $user->is_active = 1;
-        $user->active_code = rand(10000,99999);
-        $user->user_type = $request->user_type;
-        $user->password = Hash::make($request->password);
-        $user->save();
+        // $user = New User;
+        // $user->name = $request->first_name.' '.$request->last_name;
+        // $user->first_name = $request->first_name;
+        // $user->last_name = $request->last_name;
+        // $user->email = $request->email;
+        // $user->mobile = $request->mobile;
+        // $user->is_active = 1;
+        // $user->active_code = rand(10000,99999);
+        // $user->user_type = $request->user_type;
+        // $user->password = Hash::make($request->password);
+        // $user->save();
 
-        //$user = User::create($requests);
+        $user = User::create($requests);
         //$user->SendSMS();
 
         $role = Role::where('name',$request->user_type)->first();
@@ -128,9 +128,8 @@ class PassportController extends Controller
             curl_close($ch);
         }
         
-        //$user->sendEmailVerificationNotification();
-        $user->notify(new EmailVerification($user));
-
+        $user->sendEmailVerificationNotification();
+        //$user->notify(new EmailVerification($user));
 
         $token = auth()->user()->createToken('MySecret')->accessToken;
 
@@ -761,8 +760,8 @@ class PassportController extends Controller
             return \Response::json(['error'=> $arr]);
         }
 
-        //Auth::user()->sendEmailVerificationNotification();
-        auth()->user()->notify(new EmailVerification(auth()->user()));
+        Auth::user()->sendEmailVerificationNotification();
+        //auth()->user()->notify(new EmailVerification(auth()->user()));
 
         $token = auth()->user()->createToken('MySecret')->accessToken;
 
