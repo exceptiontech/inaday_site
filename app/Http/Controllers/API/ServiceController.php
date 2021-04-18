35bfe31537df30a628a9ff3f2004b0f29086525f
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-04-19 01:27:02 +0300

    Update ServiceController.php

--- app/Http/Controllers/API/ServiceController.php
+++ app/Http/Controllers/API/ServiceController.php
@@ -58,7 +58,7 @@ class ServiceController extends Controller
 
         }
 
-        return response()->json(['data' => $services->with('user','user.userdetails','user.skills','section','reviews')->latest()->paginate(10)], 200,['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        return response()->json(['data' => $services->with('user','user.userdetails','user.skills','section','reviews','reviews.user','reviews.user.userdetails')->latest()->paginate(10)], 200,['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
     }
 
@@ -74,7 +74,7 @@ class ServiceController extends Controller
         if(is_numeric($id)) {
             $service = Service::where('id',$id)->with('skills','section','user','user.userdetails','user.skills')->get();
         }else {
-            $service = Service::where('title',$id)->with('skills','section','user','user.userdetails','user.skills')->get();
+            $service = Service::where('title',$id)->with('skills','section','user','user.userdetails','user.skills','reviews','reviews.user','reviews.user.userdetails')->get();
 
         }
         return response()->json(['data' => $service], 200,['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
