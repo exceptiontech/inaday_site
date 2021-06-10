962d118fc0c9301ea1dc9ee41ffd8ecce58ab23b
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-06-10 11:58:13 +0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -92,7 +92,7 @@ class AdminController extends Controller
 
             $roles = Role::pluck('name','name')->all();
 
-            return view('admin.reports.users')->withUsers($users->latest()->get())->withRoles($roles);
+            return view('admin.reports.users')->withUsers($users->latest()->paginate(1000))->withRoles($roles);
         }
 
 
